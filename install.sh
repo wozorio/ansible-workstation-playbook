6e73aa8e523ddff3772d17bbe6fa76940322d2b3
Wellington Ozorio <well.ozorio@gmail.com>
2025-04-10 09:17:57 -0300

    Update install script

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ REPO="https://github.com/wozorio/ansible-workstation-playbook.git"
 REPO_NAME="$(basename "${REPO}" .git)"
 INSTALL_DIR="${HOME}/${REPO_NAME}"
 
+rm -rf "${INSTALL_DIR}"
 git clone "${REPO}" "${INSTALL_DIR}"
 cd "${INSTALL_DIR}"
 
