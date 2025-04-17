f6411abee45ad43e4b6f28f1ec3f425689b350ea
Wellington Ozorio <well.ozorio@gmail.com>
2025-04-17 13:05:51 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,15 +2,36 @@
 
 set -e
 
-REPO="https://github.com/wozorio/ansible-workstation-playbook.git"
-REPO_NAME="$(basename "${REPO}" .git)"
-INSTALL_DIR="${HOME}/${REPO_NAME}"
+REPO_URL="https://github.com/wozorio/ansible-workstation-playbook.git"
 
-rm -rf "${INSTALL_DIR}"
-git clone "${REPO}" "${INSTALL_DIR}"
-cd "${INSTALL_DIR}"
+is_make_installed() {
+    if command -v make &> /dev/null; then
+        echo 1
+    else
+        echo 0
+    fi
+}
 
-sudo apt update
-sudo apt install -y make
+install_make() {
+    sudo apt update
+    sudo apt install -y make
+}
 
-make bootstrap
+main() {
+    local REPO_NAME
+    REPO_NAME="$(basename "${REPO_URL}" .git)"
+
+    local INSTALL_DIR="${HOME}/${REPO_NAME}"
+
+    if [[ $(is_make_installed) -eq 0 ]]; then
+        install_make
+    fi
+
+    rm -rf "${INSTALL_DIR}"
+    git clone --depth 1 "${REPO_URL}" "${INSTALL_DIR}"
+    cd "${INSTALL_DIR}"
+
+    make bootstrap
+}
+
+main
