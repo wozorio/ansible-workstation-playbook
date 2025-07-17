c38e299c9303246c4caa02f35125967a9cfd62e2
Wellington Ozorio <well.ozorio@gmail.com>
2025-07-17 12:48:55 -0300

    x

--- install.sh
+++ install.sh
@@ -4,53 +4,57 @@ set -e
 
 readonly REPO_URL="https://github.com/wozorio/ansible-workstation-playbook.git"
 
-log_info() {
+readonly WHITE="\033[37;1m"
+readonly YELLOW="\033[33;1m"
+
+log_white() {
     local MSG="${1}"
-    echo -e "\033[37;1m ${MSG}" 1>&2
+    echo -e "${WHITE} ${MSG}" 1>&2
 }
 
-log_warning() {
+log_yellow() {
     local MSG="${1}"
-    echo -e "\033[33;1m ${MSG}" 1>&2
+    echo -e "${YELLOW} ${MSG}" 1>&2
 }
 
 is_make_installed() {
     if command -v make &>/dev/null; then
-        log_info "INFO: make is already installed"
+        log_white "INFO: make is already installed"
         echo 1
     else
-        log_info "INFO: make is not installed"
+        log_white "INFO: make is not installed"
         echo 0
     fi
 }
 
 install_make() {
-    log_info "INFO: Installing make"
+    log_white "INFO: Installing make"
     sudo apt update
     sudo apt install -y make
 }
 
 main() {
-    local REPO_NAME
-    REPO_NAME="$(basename "${REPO_URL}" .git)"
+    # local REPO_NAME
+    # REPO_NAME="$(basename "${REPO_URL}" .git)"
 
-    local INSTALL_DIR="${HOME}/${REPO_NAME}"
+    # local INSTALL_DIR="${HOME}/${REPO_NAME}"
 
-    if [[ $(is_make_installed) -eq 0 ]]; then
-        install_make
-    fi
+    # if [[ $(is_make_installed) -eq 0 ]]; then
+    #     install_make
+    # fi
 
-    rm -rf "${INSTALL_DIR}"
-    git clone --depth 1 "${REPO_URL}" "${INSTALL_DIR}"
-    cd "${INSTALL_DIR}"
+    # rm -rf "${INSTALL_DIR}"
+    # git clone --depth 1 "${REPO_URL}" "${INSTALL_DIR}"
+    # cd "${INSTALL_DIR}"
 
-    make bootstrap
+    # make bootstrap
 
-    log_info
-    log_info "INFO: Ansible has been successfully installed"
-    log_warning
-    log_warning "WARN: Before running the playbook, please restart the shell"
-    log_warning "      to ensure paths recently added to the PATH are loaded."
+    log_white "INFO: Reloading .bashrc"
+    source ${HOME}/.bashrc
+
+    log_yellow
+    log_yellow "The playbook has been successfully installed to ${INSTALL_DIR}"
+    log_yellow
 }
 
 main
